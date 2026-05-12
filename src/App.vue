a2325e657e980f6a92257629090559ba61574531
virtualp1 <zininnikita309@gmail.com>
2026-05-12 10:11:31 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>{{ sequences }}</div>
+</template>
+
+<script setup lang="ts">
+import {
+  type TextEngineSequenceStep,
+  TextEngineSequenceStepType,
+  TextEngineSequenceActionType,
+} from "text-engine/core";
+
+const sequences: TextEngineSequenceStep[] = [
+  {
+    type: TextEngineSequenceStepType.ACTION,
+    data: {
+      label: "Choose 1",
+      actions: {
+        type: TextEngineSequenceActionType.CHOOSE_ONE,
+        options: [
+          {
+            text: "First option",
+            affect: [
+              {
+                character: "Igor",
+                value: 1,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+];
+</script>
