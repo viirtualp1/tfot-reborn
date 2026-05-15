1db177fca748b59df3a0a6376594670ed1d8f586
virtualp1 <zininnikita309@gmail.com>
2026-05-15 14:21:57 +0400

    feat: add more test data

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-5">TFOT reborn</h1>
 
     <h2>Characters</h2>
-    <ul class="mb-5 bg-blue-300 rounded-lg">
+    <ul class="mb-5 rounded-lg">
       <li
         v-for="character in textEngine.state.characters"
         :key="character.id"
@@ -14,16 +14,34 @@
     </ul>
 
     <h2>Sequence</h2>
-    <ul class="bg-emerald-400 p-2 rounded-lg">
+    <ul class="p-2 rounded-lg">
       <li
         v-for="(sequence, sequenceIndex) in textEngine.state.sequence"
         :key="sequenceIndex"
         class="text-left border rounded-sm mb-2 p-2 text-lg"
       >
-        <div class="bg-emerald-200 px-2 mb-1 rounded-sm">
-          {{ sequence.data.title }}
-        </div>
-        <div>{{ sequence.data.text }}</div>
+        <template v-if="sequence.type === TextEngineSequenceStepType.DIALOG">
+          <div class="bg-emerald-200 px-2 mb-1 rounded-sm">
+            {{ sequence.data.title }}
+          </div>
+          <div>{{ sequence.data.text }}</div>
+        </template>
+        <template
+          v-else-if="sequence.type === TextEngineSequenceStepType.ACTION"
+        >
+          <div class="bg-emerald-200 px-2 mb-1 rounded-sm">
+            {{ sequence.data.label }}
+          </div>
+          <div class="flex gap-1">
+            <button
+              v-for="(option, optionIndex) in sequence.data.action.options"
+              :key="optionIndex"
+              class="bg-emerald-500 text-white flex-1 px-2 cursor-pointer mb-1 rounded-sm"
+            >
+              {{ option.text }}
+            </button>
+          </div>
+        </template>
       </li>
     </ul>
   </div>
@@ -31,6 +49,7 @@
 
 <script setup lang="ts">
 import { TextEngine } from "text-engine/core";
+import { TextEngineSequenceStepType } from "text-engine/types";
 import { formattedCharacters, sequence } from "./utils/game";
 
 const textEngine = new TextEngine({
--- src/utils/game.ts
+++ src/utils/game.ts
@@ -2,36 +2,71 @@ import {
   type Character,
   type TextEngineSequenceStep,
   TextEngineSequenceStepType,
+  TextEngineSequenceActionType,
 } from "text-engine/types";
 
 enum GameCharacters {
-  IVAN = "ivan",
+  NIKITA = "nikita",
+  YEAR = "year",
 }
 
 const characters: Record<GameCharacters, Character> = {
-  [GameCharacters.IVAN]: {
-    id: GameCharacters.IVAN,
-    name: "Ivan",
+  [GameCharacters.NIKITA]: {
+    id: GameCharacters.NIKITA,
+    name: "Nikita",
+  },
+  [GameCharacters.YEAR]: {
+    id: GameCharacters.YEAR,
+    name: "Year",
   },
 };
 
 export const formattedCharacters: Character[] = [
-  characters[GameCharacters.IVAN],
+  characters[GameCharacters.NIKITA],
 ];
 
 export const sequence: TextEngineSequenceStep[] = [
   {
     type: TextEngineSequenceStepType.DIALOG,
     data: {
-      title: characters[GameCharacters.IVAN].name,
-      text: "Hello!",
+      title: characters[GameCharacters.NIKITA].name,
+      text: `Hello, ${characters[GameCharacters.YEAR].name}!`,
     },
   },
   {
     type: TextEngineSequenceStepType.DIALOG,
     data: {
-      title: GameCharacters.IVAN,
-      text: "Hello2!",
+      title: characters[GameCharacters.YEAR].name,
+      text: `Hello, ${characters[GameCharacters.NIKITA].name}!`,
+    },
+  },
+  {
+    type: TextEngineSequenceStepType.ACTION,
+    data: {
+      label: "Choose",
+      action: {
+        type: TextEngineSequenceActionType.CHOOSE_ONE,
+        options: [
+          {
+            text: "Option 1",
+            affect: [
+              {
+                character: characters[GameCharacters.NIKITA].id,
+                value: -10,
+              },
+            ],
+          },
+          {
+            text: "Option 2",
+            affect: [
+              {
+                character: characters[GameCharacters.YEAR].id,
+                value: 10,
+              },
+            ],
+          },
+        ],
+      },
     },
   },
 ];
